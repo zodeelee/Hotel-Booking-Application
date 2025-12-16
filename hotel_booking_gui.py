4bd1dab05b596993dfaeacd3f9f13fe2a1ed1f03
Xander10Alegria <Xander10Alegria@gmail.com>
2025-12-16 16:15:17 -0600

    Add room selection and phone number buttons

--- hotel_booking_gui.py
+++ hotel_booking_gui.py
@@ -19,6 +19,15 @@ root.geometry("800x600")
 checkin_btn = tk.Button(root, text="check in", width=50, height=5)
 checkin_btn.grid(row=0, column=1, padx=150, pady=150)
 
+Room_select = tk.Button(root, text="select room", width 60, height=6)
+Room_select.grid (row=0, column= 2, padx=150, pady=150)
+
+name_id = tk.Button(root, text="select room", width 60, height=6)
+name_id.grid (row=0, column= 3, padx=150, pady=150)
+
+Phone_num = tk.Button(root, text="select room", width 60, height=6)
+phone_num.grid (row=0, column= 4, padx=150, pady=150)
+
 root.mainloop()
 
 #Create a window
